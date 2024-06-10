83bee9a58d94c4073ce3f657c2bc3d98e2bc5871
Erivan Borges <erivanborges@gmail.com>
2024-06-10 08:48:03 -0300

    Projeto ERP-Veiculos

--- src/Teste.java
+++ src/Teste.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,40 @@
  * @author Administrador
  */
 public class Teste {
+    
+    private static Passeio veiculoPasseio;
+    private static Carga veiculoCarga;
+    
+    private static Passeio[] vetVeiculoPasseio = new Passeio[5];
+    private static Carga[] vetVeiculoCarga = new Carga[5];
+    
+    private static Scanner ler;
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public static int achaVagoPasseio() {
+        for (int i = 0; i < vetVeiculoPasseio.length; i++) {
+            if (vetVeiculoPasseio[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public static int achaVagoCarga() {
+        for (int i = 0; i < vetVeiculoCarga.length; i++) {
+            if (vetVeiculoCarga[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public static void imprimeVeiculoPasseio(Passeio veiculoPasseio, int i) {
+        System.out.println("\n O veiculo de`PASSEIO armazenado no endereço.......: " + i + " (vetor do vetVeiculoPasseio)");
+        System.out.println("Cor.............................: " + veiculoPasseio.getCor());
+    }
     
 }
