db5ecd4614bc3f04c5a32113c1882aa06aa20277
Erivan Borges <erivanborges@gmail.com>
2024-06-14 13:39:26 -0300

    Projeto ERP-Veiculos

--- src/Teste.java
+++ src/Teste.java
@@ -45,6 +45,30 @@ public class Teste {
     public static void imprimeVeiculoPasseio(Passeio veiculoPasseio, int i) {
         System.out.println("\n O veiculo de`PASSEIO armazenado no endereço.......: " + i + " (vetor do vetVeiculoPasseio)");
         System.out.println("Cor.............................: " + veiculoPasseio.getCor());
+        System.out.println("Marca...........................: " + veiculoPasseio.getMarca());
+        System.out.println("Placa...........................: " + veiculoPasseio.getPlaca());
+        System.out.println("Qtdade de Rodas.................: " + veiculoPasseio.getQtdRodas());
+        System.out.println("Potência do Motor...............: " + veiculoPasseio.getMotor().getPotencia());
+        System.out.println("Qtdade Pistões do Moto..........: " + veiculoPasseio.getMotor().getQtdPistao());
+        System.out.println("Veloc. Máxima...................: " + veiculoPasseio.getVelocMax());
+        System.out.println("Qtdade de Passageiros...........: " + veiculoPasseio.getQtdPassageiros());
+    }
+    
+    public static void imprimeVeiculoCarga(Carga veiculoCarga, int i) {
+        System.out.println("\n O veiculo de CARGA armazenado no endereço " + i + " (vetor do vetVeiculoCarga)");
+        System.out.println("Cor.............................: " + veiculoCarga.getCor());
+        System.out.println("Marca...........................: " + veiculoCarga.getMarca());
+        System.out.println("Placa...........................: " + veiculoCarga.getPlaca());
+        System.out.println("Qtadade de Rodas................: " + veiculoCarga.getQtdRodas());
+        System.out.println("Veloc. Máxima...................: " + veiculoCarga.getVelocMax());
+        System.out.println("Carga Máxima....................: " + veiculoCarga.getCargaMax());
+        System.out.println("Tara............................: " + veiculoCarga.getTara());
+        System.out.println("Potência do Motor...............: " + veiculoCarga.getMotor().getPotencia());
+        System.out.println("Qtdade Pistões do motor.........: " + veiculoCarga.getMotor().getQtdPistao());
+    }
+    
+    public static Passeio cadastraVeiculoPasseio() {
+        return null;
     }
     
 }
