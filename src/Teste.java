7c3b3f70344f3a9cf9e60f353ee0f7a9cddc8ce7
Erivan Borges <erivanborges@gmail.com>
2024-07-01 07:42:51 -0300

    Projeto ERP-Veiculos

--- src/Teste.java
+++ src/Teste.java
@@ -5,29 +5,28 @@ import java.util.Scanner;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Administrador
  */
 public class Teste {
-    
+
     private static Passeio veiculoPasseio;
     private static Carga veiculoCarga;
-    
+
     private static Passeio[] vetVeiculoPasseio = new Passeio[5];
     private static Carga[] vetVeiculoCarga = new Carga[5];
-    
-    private static Scanner ler;
-    
+
+    private static Scanner ler = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println(" ");
         System.out.println("########### ATENÇÃO ###########");
         System.out.println(" ");
-        
+
         boolean continuar = true;
         int opcao = 0;
-        
+
         while (continuar) {
             System.out.println("\n\t\t\tMENU INICIAL");
             System.out.println("\t(1) Cadastrar veículo de PASSEIO");
@@ -37,78 +36,80 @@ public class Teste {
             System.out.println("\t(5) Imprimir todos os veículos de PASSEIO pela placa");
             System.out.println("\t(6) Imprimir todos os veículos de CARGA pela placa");
             System.out.println("\t(7) Sair do sistema");
-            
+
             try {
-                ler = new Scanner(System.in);
                 System.out.println("\n\tEscolha uma opção");
                 opcao = ler.nextInt();
-            } catch (NumberFormatException ex){
+            } catch (NumberFormatException ex) {
+                Scanner s = new Scanner(System.in);
                 System.out.println("Favor informar um valor inteiro - Press <ENTER>");
-                System.out.println("");
+                s.nextLine();
                 continue;
             }
-            
+
             switch (opcao) {
                 case 1:
                     for (int i = achaVagoPasseio(); i < vetVeiculoPasseio.length; i++) {
+                        Scanner s = new Scanner(System.in);
                         if (i == -1) {
                             System.out.println("\tVetor de PASSEIO está cheio! press <ENTER>");
-                            ler = new Scanner(System.in);
-                            ler.next();
+                            s.nextLine();
                             break;
                         }
-                        
+
                         veiculoPasseio = new Passeio();
                         vetVeiculoPasseio[i] = cadastraVeiculoPasseio(veiculoPasseio);
-                        
+
                         System.out.println("\nVeículo de PASSEIO armazenado na posição " + i + " do vetor.");
-                        
+
                         System.out.println("\nDeseja cadastrar outro veículo de PASSEIO? <s/n>");
-                        ler = new Scanner(System.in);
-                        String respPasseio = ler.next();
-                        
+                        String respPasseio = s.nextLine();
+
                         if (respPasseio.equalsIgnoreCase("n")) {
                             break;
                         }
                         if (achaVagoPasseio() == -1) {
                             System.out.println("\tVetor de PASSEIO está cheio! press <ENTER>");
+                            s.nextLine();
                             break;
                         }
                     }
+                    break;
                 case 2:
                     for (int i = achaVagoCarga(); i < vetVeiculoCarga.length; i++) {
+                        Scanner s = new Scanner(System.in);
                         if (i == -1) {
-                            System.out.println("\tVetor de CARGA está cheio! - press <ENTER>");
-                            ler = new Scanner(System.in);
-                            ler.next();
+                            System.out.println("\tVetor de CARGA está cheio! press <ENTER>");
+                            s.nextLine();
                         }
-                        
+
                         veiculoCarga = new Carga();
                         vetVeiculoCarga[i] = cadastraVeiculoCarga(veiculoCarga);
-                        
-                        System.out.println("\nVeículo de CARGA foi armazenado na posição " + i +" do vetor.");
-                        
+
+                        System.out.println("\nVeículo de CARGA foi armazenado na posição " + i + " do vetor.");
+
                         System.out.println("\nDeseja cadastrar outro veículo de CARGA? <s/n>");
-                        ler = new Scanner(System.in);
-                        String respCarga = ler.next();
-                        
+                        String respCarga = s.next();
+
                         if (respCarga.equalsIgnoreCase("n")) {
                             break;
                         }
                         if (achaVagoCarga() == -1) {
-                            System.out.println("\nVetor de CARGA está cheio!");
+                            System.out.println("\tVetor de CARGA está cheio! press <ENTER>");
+                            s.nextLine();
                             break;
                         }
                     }
-                case 7: 
+                    break;
+                case 7:
                     continuar = false;
                     break;
-                
+
             }
         }
-        
+
     }
-    
+
     public static int achaVagoPasseio() {
         for (int i = 0; i < vetVeiculoPasseio.length; i++) {
             if (vetVeiculoPasseio[i] == null) {
@@ -117,7 +118,7 @@ public class Teste {
         }
         return -1;
     }
-    
+
     public static int achaVagoCarga() {
         for (int i = 0; i < vetVeiculoCarga.length; i++) {
             if (vetVeiculoCarga[i] == null) {
@@ -126,7 +127,7 @@ public class Teste {
         }
         return -1;
     }
-    
+
     public static void imprimeVeiculoPasseio(Passeio veiculoPasseio, int i) {
         System.out.println("\n O veiculo de`PASSEIO armazenado no endereço.......: " + i + " (vetor do vetVeiculoPasseio)");
         System.out.println("Cor.............................: " + veiculoPasseio.getCor());
@@ -138,7 +139,7 @@ public class Teste {
         System.out.println("Veloc. Máxima...................: " + veiculoPasseio.getVelocMax());
         System.out.println("Qtdade de Passageiros...........: " + veiculoPasseio.getQtdPassageiros());
     }
-    
+
     public static void imprimeVeiculoCarga(Carga veiculoCarga, int i) {
         System.out.println("\n O veiculo de CARGA armazenado no endereço " + i + " (vetor do vetVeiculoCarga)");
         System.out.println("Cor.............................: " + veiculoCarga.getCor());
@@ -151,10 +152,8 @@ public class Teste {
         System.out.println("Potência do Motor...............: " + veiculoCarga.getMotor().getPotencia());
         System.out.println("Qtdade Pistões do motor.........: " + veiculoCarga.getMotor().getQtdPistao());
     }
-    
+
     public static Passeio cadastraVeiculoPasseio(Passeio veiculoPasseio) {
-        ler = new Scanner(System.in);
-        
         System.out.println("\n=================================");
         System.out.println("Cadastro de veículo de PASSEIO");
         System.out.println("==================================");
@@ -178,10 +177,8 @@ public class Teste {
         veiculoPasseio.getMotor().setQtdPistao(ler.nextInt());
         return veiculoPasseio;
     }
-    
+
     public static Carga cadastraVeiculoCarga(Carga veiculoCarga) {
-        ler = new Scanner(System.in);
-        
         System.out.println("\n=================================");
         System.out.println("Casdastro de veículos de CARGA");
         System.out.println("=================================");
@@ -205,7 +202,7 @@ public class Teste {
         veiculoCarga.getMotor().setPotencia(ler.nextInt());
         System.out.println("Qtdade de Pistões do Motor........:");
         veiculoCarga.getMotor().setQtdPistao(ler.nextInt());
-        return veiculoCarga;  
+        return veiculoCarga;
     }
-    
+
 }
